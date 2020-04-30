87e05b17c2f648bc1ae4f2474f4ae82e8b775038
Angelo Padron <padron891@gmail.com>
2020-04-30 20:48:35 -0300

    fixed parametro en addChapter; funcion toChapter

--- src/main/kotlin/org/unqflixabm/SeasonAppModel.kt
+++ src/main/kotlin/org/unqflixabm/SeasonAppModel.kt
@@ -4,11 +4,11 @@ import domain.Chapter
 import domain.Season
 
 class SeasonAppModel(var season: Season){
-    val id: String
-    var title: String
-    var description: String
-    var poster: String
-    var chapters: MutableList<ChapterAppModel>
+    var id: String = ""
+    var title: String = ""
+    var description: String = ""
+    var poster: String = ""
+    var chapters: MutableList<ChapterAppModel> = mutableListOf()
 
     init{
         this.id = season.id
@@ -22,7 +22,18 @@ class SeasonAppModel(var season: Season){
         return season.chapters.map{ChapterAppModel(it)}.toMutableList()
     }
 
-    fun addChapter(chapter: Chapter) = season.addChapter(chapter)
+    fun addChapter(chapter: ChapterAppModel) = season.addChapter(toChapter(chapter))
 
     fun deleteChapter(input: String) = season.deleteChapter(input)
+
+    private fun toChapter(chapterAppModel: ChapterAppModel): Chapter{
+        return Chapter(
+            chapterAppModel.id,
+            chapterAppModel.title,
+            chapterAppModel.description,
+            chapterAppModel.duration,
+            chapterAppModel.video,
+            chapterAppModel.thumbnail
+        )
+    }
 }
