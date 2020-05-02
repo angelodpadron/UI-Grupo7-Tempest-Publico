aad27f32d21c328c2b92c4d17b7ead225dd9178c
Esteban Franco <LeandroFranco97@outlook.es>
2020-05-02 00:42:47 -0300

    agrego excepcion q obliga a seleccionar serie para borrarla

--- src/main/kotlin/org/unqflixabm/appModels/UNQflixAppModel.kt
+++ src/main/kotlin/org/unqflixabm/appModels/UNQflixAppModel.kt
@@ -2,7 +2,9 @@ package org.unqflixabm.appModels
 
 import data.getUNQFlix
 import domain.UNQFlix
+import org.unqflixabm.exceptions.NonSelectSerieException
 import org.uqbar.commons.model.annotations.Observable
+import org.uqbar.commons.model.exceptions.UserException
 
 @Observable
 
@@ -10,22 +12,33 @@ class UNQflixAppModel {
 
     private var system: UNQFlix = getUNQFlix()
     var series: MutableList<SeriesAppModel> = initSeries()
-    var selectSerie:SeriesAppModel? = null // selecciona serie aunque parezca que seleccione el id
+    var selectSerie: SeriesAppModel? = null // selecciona serie aunque parezca que seleccione el id
 
     fun initSeries(): MutableList<SeriesAppModel> {
         return system.series.map { SeriesAppModel(it) }.toMutableList()
     }
-
-    fun deleteSerie(selectSerie:SeriesAppModel?){
+    fun catchDeleteException(selectSerie: SeriesAppModel?){
+        try{
+            this.deleteException(selectSerie)
+        }
+        catch(e: NonSelectSerieException){
+            throw UserException(e.message)
+        }
+    }
+    fun deleteException(selectSerie: SeriesAppModel?){
+        if (selectSerie == null) {
+            throw NonSelectSerieException("Please select a serie before continue")
+        }
+    }
+    fun deleteSerie(selectSerie: SeriesAppModel?) {
         if (selectSerie != null) {
             system.deleteSerie(selectSerie.id)
         }
         series = initSeries()
-
     }
 
-    fun getSerie(selectSerie: String?): SeriesAppModel?{
-        return series.find {it.id == selectSerie }
+    fun getSerie(selectSerie: String?): SeriesAppModel? {
+        return series.find { it.id == selectSerie }
     }
 
     //ALTA
---
+++ src/main/kotlin/org/unqflixabm/exceptions/NonSelectSerieException.kt
@@ -0,0 +1,7 @@
+package org.unqflixabm.exceptions
+
+import kotlin.Exception
+
+class NonSelectSerieException(message : String): Exception(message) {
+
+}
