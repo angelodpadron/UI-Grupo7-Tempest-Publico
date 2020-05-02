3b054236b28e7ee84d4e3fc28b5bf4f5256ab124
Angelo Padron <padron891@gmail.com>
2020-05-02 18:37:05 -0300

    only id and title

--- src/main/kotlin/org/unqflixabm/appModels/ContentAppModel.kt
+++ src/main/kotlin/org/unqflixabm/appModels/ContentAppModel.kt
@@ -2,6 +2,15 @@ package org.unqflixabm.appModels
 
 import domain.Content
 
-class ContentAppModel (content: Content) {
+class ContentAppModel (model: Content){
+
+    var id : String = ""
+    var title : String = ""
+
+    init {
+        id = model.id
+        title = model.title
+
+    }
 
 }
