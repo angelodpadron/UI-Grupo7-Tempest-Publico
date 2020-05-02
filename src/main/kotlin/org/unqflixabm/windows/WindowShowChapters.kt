bff05994f103439e5316418bd7316177ce223eca
NahuelIglesias <nahueliglesias1999@gmail.com>
2020-05-02 00:08:19 -0300

    Esqueleto de WindowShowChapters, confirmar formato y configurar TODOs

--- src/main/kotlin/org/unqflixabm/windows/WindowShowChapters.kt
+++ src/main/kotlin/org/unqflixabm/windows/WindowShowChapters.kt
@@ -1,4 +1,53 @@
 package org.unqflixabm.windows
 
-class WindowShowChapters {
-}
+import org.unqflixabm.appModels.SeasonAppModel
+import org.unqflixabm.appModels.ChapterAppModel
+import org.uqbar.arena.widgets.Panel
+import org.uqbar.arena.windows.SimpleWindow
+import org.uqbar.arena.windows.WindowOwner
+import org.uqbar.arena.kotlin.extensions.*
+import org.uqbar.arena.widgets.*
+
+class WindowShowChapters (owner: WindowOwner, seasonAppModel: SeasonAppModel):
+    SimpleWindow<SeasonAppModel>(owner, seasonAppModel) {
+        override fun addActions(p0: Panel?) {
+        }
+
+        override fun createFormPanel(p0: Panel) {
+            Label(p0) with {
+                text = "Season:" + modelObject.title
+            }
+
+            table<ChapterAppModel>(p0) {
+                title = "Chapters:"
+                bindItemsTo("chapters")
+                //TODO: bindSelectionTo
+                column {
+                    title = "#"
+                    fixedSize = 75
+                    bindContentsTo("id") //TODO: probablemente haya que sacarlo
+                }
+                column {
+                    title = "Title"
+                    fixedSize = 300
+                    bindContentsTo("title")
+                }
+                column {
+                    title = "Duration"
+                    fixedSize = 75
+                    bindContentsTo("duration")
+                }
+            }
+
+            Panel(p0) with {
+                Button(it) with {
+                    caption = "Add new chapter"
+                    //TODO: onClick
+                }
+                Button(it) with {
+                    caption = "Modify chapter"
+                    //TODO: onClick
+                }
+            }
+        }
+    }
