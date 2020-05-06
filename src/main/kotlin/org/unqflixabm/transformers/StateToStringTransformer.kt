a43a98d7a9775dc6e23631995d07234e10822b87
NahuelIglesias <nahueliglesias1999@gmail.com>
2020-05-05 21:34:22 -0300

    Pasaje a trabajo con listas de StateToStringTransformer (no funciona)

--- src/main/kotlin/org/unqflixabm/transformers/StateToStringTransformer.kt
+++ src/main/kotlin/org/unqflixabm/transformers/StateToStringTransformer.kt
@@ -13,23 +13,48 @@ class StateToStringTransformer : ValueTransformer<ContentState, String> {
     override fun getModelType() = ContentState::class.java
     override fun getViewType() = String::class.java
 
-    override fun modelToView(valueFromModel: ContentState): String {
-        var stateStr: String = valueFromModel.toString()
-        if (stateStr.contains("Available")) {
-            stateStr = "Available"
-        } else if (stateStr.contains("Unavailable")) {
-            stateStr = "Unavailable"
+    //override fun modelToView(valueFromModel: ContentState): String {
+    //    var stateStr: String = valueFromModel.toString()
+    //    if (stateStr.contains("Available")) {
+    //        stateStr = "Available"
+    //    } else if (stateStr.contains("Unavailable")) {
+    //        stateStr = "Unavailable"
+    //    }
+    //    return stateStr
+    //}
+
+    override fun modelToView(valueFromView: List<ContentState>): List<String> {
+        var statesStr: List<String> = mutableListOf()
+        for (stateObj in valueFromView) {
+            var stateStr: String = stateObj.toString()
+            if (stateStr.contains("Available")) {
+                statesStr.add("Available")
+            } else {
+                statesStr.add("Unavailable")
+            }
         }
-        return stateStr
+        return statesStr
     }
 
-    override fun viewToModel(valueFromView: String): ContentState {
-        var stateObj: ContentState
-        if (valueFromView == "Available") {
-            stateObj = Available()
-        } else {
-            stateObj = Unavailable()
+    //override fun viewToModel(valueFromView: String): ContentState {
+    //    var stateObj: ContentState
+    //    if (valueFromView == "Available") {
+    //        stateObj = Available()
+    //    } else {
+    //        stateObj = Unavailable()
+    //    }
+    //    return stateObj
+    //}
+
+    override fun viewToModel(valueFromView: List<String>): List<ContentState> {
+        var statesObj: List<ContentState> = mutableListOf()
+        for (stateStr in valueFromView){
+            if (stateStr == "Available") {
+                statesObj.add(Available())
+            } else {
+                statesObj.add(Unavailable())
+            }
         }
-        return stateObj
+        return statesObj
     }
 }
