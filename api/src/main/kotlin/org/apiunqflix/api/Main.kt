0136ca577f0a4f6304a07c9b79e35ceccce5e6bf
Angelo Padron <padron891@gmail.com>
2020-05-24 19:38:37 -0300

    setup jwt; setup backend

--- api/src/main/kotlin/org/apiunqflix/api/Main.kt
+++ api/src/main/kotlin/org/apiunqflix/api/Main.kt
@@ -1,7 +1,10 @@
 package org.apiunqflix.api
 
+import data.getUNQFlix
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
 import io.javalin.core.util.RouteOverviewPlugin
+import org.apiunqflix.controller.UserController
 
 fun main() {
     val app = Javalin.create {
@@ -10,5 +13,21 @@ fun main() {
         it.enableCorsForAllOrigins()
     }
     app.start(7000)
+
+    val unqflix = getUNQFlix()
+    val tokenJWT = TokenJWT()
+
+    //controllers
+    val userController = UserController(unqflix, tokenJWT)
+
+    app.routes{
+        // usuarios
+        path("/register"){
+            post(userController::register)
+        }
+        path("/login"){
+            post(userController::login)
+        }
+    }
 
 }
