62ec200c8c4a7e084127dbe5d771864459b5199a
Angelo Padron <padron891@gmail.com>
2020-05-26 11:35:09 -0300

    login; register; viewcontent; viewuser

--- api/src/main/kotlin/org/apiunqflix/mapper/UNQFlixMapper.kt
+++ api/src/main/kotlin/org/apiunqflix/mapper/UNQFlixMapper.kt
@@ -1,5 +1,8 @@
 package org.apiunqflix.mapper
 
-data class UserViewMapper (val name: String, val email: String, val creditCard: String, val image: String)
-data class LoginUserMapper (val email: String, val password: String)
+data class UserViewMapper(val name: String, val image: String, val favorites: MutableList<ContentViewMapper>, val lastSeen: MutableList<ContentViewMapper>)
+data class ContentViewMapper(val id: String, val description: String, val title: String, val state: Boolean)
+
+data class LoginUserMapper (val email: String? = null, val password: String? = null)
+
 data class UserRegisterMapper (val name: String?, val email: String?, val password: String?, val creditCard: String?, val image: String?)
