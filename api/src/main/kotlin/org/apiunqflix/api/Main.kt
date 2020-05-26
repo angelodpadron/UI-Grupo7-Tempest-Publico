13df38eeb79693f1b67988100676b7c8f9335e52
Angelo Padron <padron891@gmail.com>
2020-05-26 11:31:18 -0300

    added role; app routes refactor

--- api/src/main/kotlin/org/apiunqflix/api/Main.kt
+++ api/src/main/kotlin/org/apiunqflix/api/Main.kt
@@ -3,30 +3,40 @@ package org.apiunqflix.api
 import data.getUNQFlix
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
+import io.javalin.core.security.Role
 import io.javalin.core.util.RouteOverviewPlugin
 import org.apiunqflix.controller.UserController
 
+enum class Rol: Role {
+    ANYONE, USER, ADMIN
+}
+
 fun main() {
     val app = Javalin.create {
-        it.defaultContentType ="aplication/json"
+        it.defaultContentType ="application/json"
         it.registerPlugin(RouteOverviewPlugin("/routes"))
         it.enableCorsForAllOrigins()
     }
     app.start(7000)
 
-    val unqflix = getUNQFlix()
+    val backend = getUNQFlix()
     val tokenJWT = TokenJWT()
 
     //controllers
-    val userController = UserController(unqflix, tokenJWT)
+    val userController = UserController(backend, tokenJWT)
 
     app.routes{
         // usuarios
+        path("/login"){
+            post(userController::login)
+        }
         path("/register"){
             post(userController::register)
         }
-        path("/login"){
-            post(userController::login)
+        path("/user"){
+            path("/:id"){
+                get(userController::getUser)
+            }
         }
     }
 
