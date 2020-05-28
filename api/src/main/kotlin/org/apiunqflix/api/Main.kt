daefeccebd5128833a475d17cb4e914aa78272ae
Esteban Franco <LeandroFranco97@outlook.es>
2020-05-27 21:44:16 -0300

    arregle path de search

--- api/src/main/kotlin/org/apiunqflix/api/Main.kt
+++ api/src/main/kotlin/org/apiunqflix/api/Main.kt
@@ -53,17 +53,16 @@ fun main() {
             }
         }
         path("/banners"){
-            get(unqFlixController::getAllBanners, mutableSetOf<Role>(Rol.INVITED))
+            get(unqFlixController::getAllBanners, mutableSetOf<Role>(Rol.INVITED,Rol.ACTIVE_USER))
         }
         path("/content") {
-            get(unqFlixController::getAvailableContent, mutableSetOf<Role>(Rol.INVITED))
+            get(unqFlixController::getAvailableContent, mutableSetOf<Role>(Rol.INVITED,Rol.ACTIVE_USER))
             path("/:contentId") {
-                get(unqFlixController::getContentById, mutableSetOf<Role>(Rol.INVITED))
+                get(unqFlixController::getContentById, mutableSetOf<Role>(Rol.INVITED,Rol.ACTIVE_USER))
             }
         }
-        //path("/search?text=:text") {
-        //    get(unqFlixController::searchText, mutableSetOf<Role>(Rol.INVITED))
-        //}
-
+        path("/search") {
+            get(unqFlixController::searchText, mutableSetOf<Role>(Rol.INVITED))
+        }
     }
 }
