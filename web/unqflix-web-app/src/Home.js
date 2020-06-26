444c59b5435893f0403f9a5f09d5d69beccaed6b
Angelo Padron <padron891@gmail.com>
2020-06-26 20:07:44 -0300

    update home

--- web/unqflix-web-app/src/Home.js
+++ web/unqflix-web-app/src/Home.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-class Home extends React.Component{
-	constructor(){
-		super();
+const Home = () => {
+	const [banners, setBanners] = useState(undefined);
+	const [unnautorized, setUnnautorized] = useState(false);
+
+	//localhost
+	const host = 'http://localhost:7000'		
+
+	useEffect(() => {
+		axios.get(host.concat('/banners'))
+		.then(response => setBanners(response.data))
+		.catch(() => setUnnautorized(true))		
+		
+
+	}, []);	
+
+	if (unnautorized) {
+		return (
+			<div>
+				Error al acceder a la API
+			</div>
+		);
 	}
 
-	render(){
-		return(
-		<div className="container">
+	return(
+		<>
 			<div className="topnav">
 				<a href="#">UNQFlix</a>
 				<div className="search-container">
@@ -18,14 +36,16 @@ class Home extends React.Component{
 				</div>
 			</div>
 			<div className="content">
-				<div className="banners-carousel"></div>
+				<div className="banners-carousel">Carousel placeholder</div>
 				<div className="viewed">Recently watched</div>
 				<div className="favourites">Favourites</div>
-			</div>
-		</div>
-	)	
-	}
-	
+				<div className="banners">Resto de contenidos</div>				
+			</div>			
+		</>
+		
+	);	
 }
+	
+
 
 export default Home;
