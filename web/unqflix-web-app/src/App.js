bdac8910e593eca61a10dd0fa8037c1343792bfd
Angelo Padron <padron891@gmail.com>
2020-07-02 15:14:37 -0300

    update app routing

--- web/unqflix-web-app/src/App.js
+++ web/unqflix-web-app/src/App.js
@@ -5,17 +5,24 @@ import Landing from './Landing'
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
+import Search from './Search'
+
+import NotFoundPage from './NotFoundPage'
 
 export default class App extends React.Component{
-    
-    
+
     render(){
         return(
+            
             <Router>
-                <Route exact path= '/' component={Landing}/>
-                <Route exact path= '/login' component={Login}/>
-                <Route exact path= '/signup' component={Register}/>
-                <Route exact path= '/home' component={Home}/>
+                <Switch>            
+                    <Route exact path= '/' component={Landing}/>
+                    <Route exact path= '/login' component={Login}/>
+                    <Route exact path= '/signup' component={Register}/>
+                    <Route exact path= '/home' component={Home}/>
+                    <Route exact path= '/search' component={Search}/>
+                    <Route path='*'  component={NotFoundPage}/>
+                </Switch>
             </Router>
         );
     }
