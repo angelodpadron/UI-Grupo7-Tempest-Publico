4036cf0e1f73148baeb29db4de554a2d433fec8f
Esteban Franco <LeandroFranco97@outlook.es>
2020-07-03 00:13:43 -0300

    movie,videoPlayer,css Movie

---
+++ web/unqflix-web-app/src/Movie.js
@@ -0,0 +1,55 @@
+import React, { useEffect,useState } from 'react'
+import Api from './Api';
+import { Redirect } from 'react-router-dom';
+import './Movie.css'
+
+const Movie = (props) => {
+	const [movie, setMovie] = useState(undefined);	
+  const [id] = useState(props.match.params.id);
+  const [loading, setLoading] = useState(true)
+  
+
+  useEffect(() => {      
+
+      Api.getContentId(id)
+      .then(response => {
+        setMovie(response.data)
+        setLoading(false)})      
+  })
+
+
+  if (loading){
+    return(
+      <h1>Loading...</h1>
+    )
+  }
+
+  const poster = (contentData, id) => {
+		let url = '/content/'.concat(id);
+		return (
+      <a href={url}>
+        <div className ="relcontent"> 		
+			<img className="contentIcon" src={contentData.poster} alt={contentData.title}/>
+      </div>
+      </a>		
+		);
+	}
+   
+
+return (          
+      <div className ="containerMovie">
+          <div className ="featuresMovie">
+            <p className ="title"><h3>{movie.title}</h3></p>
+            <img className ="poster" src={movie.poster}/>
+            <p className ="description">{movie.description}</p>
+            <button className="reproduceButn"></button>
+          </div>
+        <p className ="relatedContentTitle"><h4>Related Content</h4></p>
+        <div className ="relatedContent">
+          {movie.relatedContent.map( cont =>poster(cont,cont.id))}           
+        </div>
+    </div>
+   );  
+}
+
+export default Movie;
