740221cb42818c2a8cdc4b25e0e47b7809c3c0ee
Esteban Franco <LeandroFranco97@outlook.es>
2020-07-07 21:09:08 -0300

    movie and videoPlayer

--- web/unqflix-web-app/src/Movie.js
+++ web/unqflix-web-app/src/Movie.js
@@ -1,20 +1,22 @@
 import React, { useEffect,useState } from 'react'
 import Api from './Api';
-import { Redirect } from 'react-router-dom';
-import './Movie.css'
+import { Redirect ,Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Content.css';
 
-const Movie = (props) => {
+
+export default function Movie (props)  {
 	const [movie, setMovie] = useState(undefined);	
   const [id] = useState(props.match.params.id);
   const [loading, setLoading] = useState(true)
   
 
-  useEffect(() => {
-    
-      const currentToken = {headers: {"Authentication": sessionStorage.getItem("currentUser")}}
+  useEffect(() => {      
 
-      Api.getContentId(id, currentToken)
-      .then(response => {
+    const currentToken = {headers: {"Authentication": sessionStorage.getItem("currentUser")}}
+
+    Api.getContentId(id, currentToken)      
+    .then(response => {
         setMovie(response.data)
         setLoading(false)})      
   })
@@ -35,31 +37,46 @@ const Movie = (props) => {
 		if (id.includes("ser")){
 			url = '/serie/'.concat(id)
     }
-    
 		return (
       <a href={url}>
         <div className ="relcontent"> 		
-			<img className="contentIcon" src={contentData.poster} alt={contentData.title}/>
+        <img className="contentIcon" src={contentData.poster} alt={contentData.title}/>
       </div>
       </a>		
 		);
 	}
    
 
-return (          
-      <div className ="containerMovie">
-          <div className ="featuresMovie">
-            <p className ="title"><h3>{movie.title}</h3></p>
-            <img className ="poster" src={movie.poster}/>
-            <p className ="description">{movie.description}</p>
-            <button className="reproduceButn"></button>
-          </div>
-        <p className ="relatedContentTitle"><h4>Related Content</h4></p>
-        <div className ="relatedContent">
-          {movie.relatedContent.map( cont =>poster(cont,cont.id))}           
+return (
+        <>            
+        <div className="topnav">
+            <Link to='/home'>UNQFlix</Link>					
+            <div className="user-dropdown">
+                <button class="dropbtn"> Sign out</button>						
+            </div>						
+            <div className="search-container">
+                <form >
+                    <button type="submit">Go</button>
+                    <input type="text" name="text" placeholder="Search..."/>
+                </form>
+            </div>
+                                                            
         </div>
-    </div>
-   );  
+        <div className ="jumbotron jumbotron-fluid">
+            <img  src={movie.poster}/>
+            <div className="container">
+              <h1 className ="display-4">{movie.title} </h1>
+              <p className ="lead">{movie.description}</p>
+              <Link to={`/videoplayer/${movie.id}`}className="btn btn-primary"> <img src={process.env.PUBLIC_URL + '/image/boton-de-reproduccion.svg'} className="reproduceBtnIcon" width="250px" /> Play </Link>
+            </div>
+        </div>
+        <div className="container">
+          <p className ="relatedContentTitle"><h4>Related Content</h4></p>
+          <div className ="relatedContent">
+              {movie.relatedContent.map( cont =>poster(cont,cont.id))}           
+          </div>
+        </div>
+        </>
+      );  
 }
 
-export default Movie;
--- web/unqflix-web-app/src/videoPlayer.js
+++ web/unqflix-web-app/src/videoPlayer.js
@@ -1,41 +1,68 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
+import { Link } from 'react-router-dom';
 import ReactPlayer from 'react-player'
+import ReactJWPlayer from 'react-jw-player'
 import './Home.css'
+import Api from './Api';
 
-class VideoPlayer extends React.Component{
+export default function Movie(props){
 
-    constructor(props){
-        super(props);
+	const [content, setContent] = useState(undefined);	
+  const [id] = useState(props.match.params.id);
+  const [loading, setLoading] = useState(true)
+  
+
+  useEffect(() => {      
+
+    const currentToken = {headers: {"Authentication": sessionStorage.getItem("currentUser")}}
+
+    Api.getContentId(id, currentToken)      
+    .then(response => {
+        setContent(response.data)
+        setLoading(false)})      
+    },[])
+
+
+    if (loading){
+      return(
+        <h1>Loading...</h1>
+      )
+    }
     
-        this.state = {
-          url:""
-        }
-      }
-    
- render( ){
-     return (
-       
-
-        <div className = "reproductor" style={{width:'100%',height:'100%',position:"absolute"} }>
-            <ReactPlayer
-                url = "https://www.youtube.com/watch?v=fBNpSRtfIUA"
+   return (
+        <>
+          <div className="topnav">
+             <Link to='/home'>UNQFlix</Link>					
+             <div className="user-dropdown">
+                 <button class="dropbtn"> Sign out</button>						
+             </div>						
+             <div className="search-container">
+                 <form >
+                     <button type="submit">Go</button>
+                     <input type="text" name="text" placeholder="Search..."/>
+                 </form>
+             </div>
+          </div>
+          <div className = "reproductor" style={{width:'100%',height:'100%',position:"absolute"} }>
+          <ReactJWPlayer
+          playerScript=  {content.video}
+          />   
+             
+             {/* <ReactPlayer
+                url = {content.video}
                 width = '100%'
                 height = '90%'
                 config={{
-                    youtube: {
-                      playerVars: {
-                                    showinfo:0,
-                                    rel:0,
-                                    controls:1
-                                  }
-                             }
-                      }
-                }
-            />
-        </div>
-     )
- }
-
+                         youtube: {
+                         playerVars: {
+                                      showinfo:0,
+                                      rel:0,
+                                     controls:1
+                                     }}
+                      }}
+              /> */}
+          </div>
+      </>
+   )
 }
 
-export default VideoPlayer;
